87ca098d52ac34bfb03d7c81c074c26f573709cd
Emmanuel Adje Frimpong <tydevelopes@gmail.com>
2023-06-26 02:41:36 +0000

    added task 3

---
+++ 3-quick_sort.c
@@ -0,0 +1,69 @@
+#include "sort.h"
+
+/**
+ * swap - swap two int
+ * @a: int
+ * @b: int
+ */
+void swap_var(int *a, int *b)
+{
+	int tmp;
+
+	tmp = *a;
+	*a = *b;
+	*b = tmp;
+}
+/**
+ * partition - Partition an array and using pivot
+ * @array: Array
+ * @low: int
+ * @high: int
+ * @size: size of array (size_t)
+ * Return: index of pivote (int)
+ */
+int partition(int *array, int low, int high, size_t size)
+{
+	int pivot = array[high];
+	int x = low - 1, y;
+
+	for (y = low; y <= high; y++)
+	{
+		if (array[y] <= pivot)
+		{
+			x++;
+			if (x != y)
+			{
+				swap_var(&array[x], &array[y]);
+				print_array(array, size);
+			}
+		}
+	}
+	return (x);
+}
+/**
+ * lomuto_qsort - Sorting Recursively an Array
+ * @array: Array to be sorted
+ * @low: The lowest value of the array
+ * @high: highest value of the array
+ * @size: Size of The Array
+ */
+void _qsort(int *array, int low, int high, size_t size)
+{
+	int i;
+
+	if (low < high)
+	{
+		i = partition(array, low, high, size);
+		_qsort(array, low, i - 1, size);
+		_qsort(array, i + 1, high, size);
+	}
+}
+/**
+ * quick_sort - Quick Sort Algorithme using lomuto partition
+ * @array: Array to sort
+ * @size: Size of The Array
+ */
+void quick_sort(int *array, size_t size)
+{
+	_qsort(array, 0, size - 1, size);
+}
