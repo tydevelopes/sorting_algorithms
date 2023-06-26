a582dab5ec56af4ae3e5979ac31594060ca2871f
Emmanuel Adje Frimpong <tydevelopes@gmail.com>
2023-06-26 02:50:09 +0000

    deleted unneeded content in  sort.h

--- sort.h
+++ sort.h
@@ -26,22 +26,6 @@ void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
 void quick_recursion(int *array, int left, int right, size_t size);
 int partition(int *array, int left, int right, size_t size);
-void shell_sort(int *array, size_t size);
-void switch_nodes(listint_t **list, listint_t **p);
-void cocktail_sort_list(listint_t **list);
-void counting_sort(int *array, size_t size);
-void merge_sort(int *array, size_t size);
-void merge_recursion(int *arr, int *array, size_t left, size_t right);
-void merge_subarray(int *arr, int *array, size_t left,
-		    size_t middle, size_t right);
-void heap_sort(int *array, size_t size);
-void heapify(int *array, size_t s, size_t root, size_t size);
-void radix_sort(int *array, size_t size);
-void count_sort_LSD(int *array, size_t size, size_t lsd);
-void quick_sort_hoare(int *array, size_t size);
-void bitonic_sort(int *array, size_t size);
-void bitonic_recursion(int *array, int l, int r, int direction, size_t size);
-void bitonic_merge(int *array, int l, int r, int direction);
 void swap(listint_t *a, listint_t *b, listint_t **head);
 
 #endif
